8fc9b00fe7db9cee898e1cfa153d68f4d42fe766
Jesús Velarde <velardejesushsd@gmail.com>
2026-09-04 14:32:56 -0300

    Day 3 Navigator

--- lib/main.dart
+++ lib/main.dart
@@ -4,14 +4,23 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
-  State<MyApp> createState() => _MyAppState();
+  Widget build(BuildContext context) {
+    return MaterialApp(home: PantallaPrincipal());
+  }
 }
 
-class _MyAppState extends State<MyApp> {
+class PantallaPrincipal extends StatefulWidget {
+  const PantallaPrincipal({super.key});
+
+  @override
+  State<PantallaPrincipal> createState() => _PantallaPrincipalState();
+}
+
+class _PantallaPrincipalState extends State<PantallaPrincipal> {
   int contador = 0;
 
   @override
@@ -20,22 +29,69 @@ class _MyAppState extends State<MyApp> {
       home: Scaffold(
         appBar: AppBar(title: Text('Mi primera app')),
         body: Center(
+          child: Container(
+            padding: const EdgeInsets.all(20),
+            decoration: BoxDecoration(
+              color: Colors.blueAccent,
+              borderRadius: BorderRadius.circular(12),
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      contador++;
+                    });
+                    print(contador);
+                  },
+                  child: const Text('tocame'),
+                ),
+                SizedBox(height: 10),
+                Text('La cantidad de veces que hiciste click en el boton es '),
+                SizedBox(height: 10),
+                Text('$contador'),
+
+                SizedBox(height: 15),
+
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => SegundaPantalla(),
+                      ),
+                    );
+                  },
+                  child: Text('Navegar a la segunda pantalla'),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class SegundaPantalla extends StatelessWidget {
+  const SegundaPantalla({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Segunda pantalla')),
+      body: Center(
+        child: Container(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               ElevatedButton(
                 onPressed: () {
-                  setState(() {
-                    contador++;
-                  });
-                  print(contador);
+                  Navigator.pop(context);
                 },
-                child: const Text('tocame'),
+                child: Text('Volver a la pantalla principal'),
               ),
-              SizedBox(height: 10),
-              Text('La cantidad de veces que hiciste click en el boton es '),
-              SizedBox(height: 10),
-              Text('$contador'),
             ],
           ),
         ),
